a8025edeae106b823f32656905d92a88e60c75b3
Paweł Jaroszek <pawel.jaroszek@digit-al.pl>
2019-11-25 11:24:13 +0100

    Added timer in the project to write to log on interval 60 secounds

--- MyNewService/MyNewService.cs
+++ MyNewService/MyNewService.cs
@@ -1,10 +1,14 @@
 ﻿using System.Diagnostics;
 using System.ServiceProcess;
+using System.Timers;
+
 
 namespace MyNewService
 {
     public partial class MyNewService : ServiceBase
     {
+        private int eventId = 1;
+
         public MyNewService()
         {
             InitializeComponent();
@@ -19,10 +23,22 @@ namespace MyNewService
         protected override void OnStart(string[] args)
         {
             eventLog1.WriteEntry("In OnStart");
+            Timer timer = new Timer
+            {
+                Interval = 60000
+            };
+            timer.Elapsed += new ElapsedEventHandler(this.OnTimer);
+            timer.Start();
+        }
+        public void OnTimer(object sender, ElapsedEventArgs args)
+        {
+            // TODO: Insert monitoring activities here.
+            eventLog1.WriteEntry("Monitoring the System", EventLogEntryType.Information, eventId++);
         }
 
         protected override void OnStop()
         {
+            eventLog1.WriteEntry("In OnStop.");
         }
     }
 }
